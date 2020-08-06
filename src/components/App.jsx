41b142814dd8e08975bc02230bbedd99a0e8da03
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-06 21:23:39 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Grid from "@material-ui/core/Grid";
+
+import Button from "@material-ui/core/Button";
+import Topbar from "./Topbar";
+
+function App() {
+  return <Topbar />;
+}
+
+export default App;
---
+++ src/components/Topbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Grid, Toolbar, AppBar, Button } from "@material-ui/core/";
+
+import Logo from "../images/logo.svg";
+function Topbar() {
+  return (
+    <AppBar color="white">
+      <Toolbar>
+        <Grid container direction="row" className="top-bar">
+          <img alt="logo" src={Logo} className="logo" />
+
+          <Button className="top-bar-btn-style">Try it free</Button>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Topbar;
