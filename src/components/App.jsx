aac4750faacf3e655abe4529cb74181641b1cbed
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-07 18:21:22 +0200

    footer section added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-
-import { Grid, Button } from "@material-ui/core/";
+import { Grid, Button, TextField } from "@material-ui/core/";
 
 //import Button from "@material-ui/core/Button";
 import Topbar from "./Topbar";
 import Screen from "../images/screen-mockups.svg";
 import Messages from "../images/icon-messages.svg";
 import Communities from "../images/icon-communities.svg";
-//import Growtogether from "../images/illustration-grow-together.svg";
-//import Flowing from "../images/illustration-flowing-conversation.svg";
-//import Users from "../images/illustration-your-users.svg";
+import Whitelogo from "../images/logo-white.svg";
+import Phone from "../images/icon-phone.svg";
+import Email from "../images/icon-email.svg";
 
 import Cards from "./Cards";
 import Intro from "./Intro";
 import features from "./features";
 import Features from "./Features";
 import Cta from "./Cta";
+
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
 
 function createFeat(feature) {
   return (
@@ -65,6 +68,52 @@ function App() {
         title="Ready To Build Your Community?"
         buttonText="Get Started For Free"
       />
+      <Grid className="footer ">
+        <Grid container direction="row" className="reverse-order">
+          <Grid item md={6} className="footer-contacts">
+            <img alt="logowhite" src={Whitelogo} />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+              nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.
+            </p>
+            <Grid container direction="row" className="footer-contacts-row">
+              <img alt="phone" src={Phone} />
+              <h5> +1-543-123-4567</h5>
+            </Grid>
+            <Grid container direction="row" className="footer-contacts-row">
+              <img alt="phone" src={Email} />
+              <h5> example@huddle.com</h5>
+            </Grid>
+          </Grid>
+          <Grid item md={6} className="newsletter-footer ">
+            <h3> Newsletter</h3>
+            <p>
+              {" "}
+              To recieve tips on how to grow your community, sign up to our
+              weekly newsletter. We'll never send you spam or pass on your email
+              address{" "}
+            </p>
+            <Grid container direction="row">
+              <Grid item md={8} xs={12}>
+                <TextField
+                  id="filled-basic"
+                  label="email@example.com"
+                  variant="filled"
+                  className="newsletter-input"
+                />
+              </Grid>
+              <Grid item md={4} xs={12}>
+                <Button className="footer-btn">Subscribe</Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container direction="row" className="icons-footer">
+          <FacebookIcon className="footer-social-icon" />
+          <InstagramIcon className="footer-social-icon"/>
+          <TwitterIcon className="footer-social-icon"/>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,7 +5,7 @@ import { Grid, Toolbar, AppBar, Button } from "@material-ui/core/";
 import Logo from "../images/logo.svg";
 function Topbar() {
   return (
-    <AppBar position="fixed" color="white">
+    <AppBar position="fixed" color="inherit">
       <Toolbar>
         <Grid container direction="row" className="top-bar">
           <img alt="logo" src={Logo} className="logo" />
